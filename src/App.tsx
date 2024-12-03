bba24a510c4d681c58469c5bca5585b31ac36308
Pierrick Hauguel <pierrickhauguel@outlook.com>
2024-12-03 13:47:58 +0100

    add planet list

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,45 @@
+import { useEffect, useState } from "react";
 import "./App.css";
-import Card, { url } from "./components/CardComponent";
 
+type Planet = {
+  name: string;
+  rotation_period: string;
+  orbital_period: string;
+  diameter: string;
+  climate: string;
+  gravity: string;
+  terrain: string;
+  surface_water: string;
+  population: string;
+  residents: string[];
+  films: string[];
+  created: string;
+  edited: string;
+  url: string;
+};
 function App() {
-  console.log(url);
+  const [refresh, setRefresh] = useState(false);
+  const [planets, setPlanets] = useState<Planet[]>([]);
+
+  useEffect(() => {
+    fetch("https://swapi.dev/api/planets")
+      .then((response) => response.json())
+      .then((data) => setPlanets(data.results));
+  }, [refresh]);
+
   return (
     <div>
       <p>bienvenue</p>
-      <Card title="title" content="content"></Card>
+      <button onClick={() => setRefresh(!refresh)}>Refresh</button>
+      {planets.map((planet) => {
+        return (
+          <div key={planet.url}>
+            <p>name : {planet.name}</p>
+          </div>
+        );
+      })}
+      <button>previous</button>
+      <button>next</button>
     </div>
   );
 }
